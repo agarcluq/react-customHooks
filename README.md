e55ac79d51239344e7c07f0ea9acd6e244618571
agarcluq <agarcluq@everis.com>
2021-04-19 08:24:32 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-vadw6d
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-vadw6d)
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+
+  const useFriendStatus = (id) =>{
+    
+  }
+  return (
+    <div>
+      <p>{isOnline ? "Online" : "Offline"}</p>
+      <button onClick = {handler}>Connect friend</button>
+    </div>
+  );
+}
